f8b19f8a4f3877ddd1725e862491d6968e2d7902
Shreyash-Pathare <yaashpathare1914@gmail.com>
2026-03-01 21:05:48 +0530

    feat: add core app structure, components and shared types

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+
+import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
+
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">ReadySetHire</h2>
+        </Link>
+
+        <form action={signOut}>
+          <button type="submit" className="btn-secondary">
+            Logout
+          </button>
+        </form>
+      </nav>
+
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
